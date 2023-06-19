54f81a702dc7d5d323868d85daf112d645970435
mercy oyor <mercyoyor@hotmail.com>
2023-06-19 14:39:40 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-
 /**
- * main - prints the lowercase alphabet in reverse.
- * folowwes by a new line
- * Return: Always 0 (Success)
+ * main - print the lowercase alphabet as mirror
+ * Return: Always 0
  */
+
 int main(void)
-[
-	char ch:
-	for (ch . "z": ch >- "a": ch--)
-	[
-		putchar(ch):
-	]
-	putchar("\n")
-	return (0):
-]
+{
+	char c;
+
+	c = 'z';
+	while 
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return(0);
+}

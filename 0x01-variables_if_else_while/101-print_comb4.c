24b2120a54fb8e2341f9f8387f7fcda8c68605c4
mercy oyor <mercyoyor@hotmail.com>
2023-06-20 00:39:41 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,32 +1,40 @@
 #include <stdio.h>
-
 /**
- * main - prints all possible different combination of three digits.
- * Return: Always 0 (Success)
+ * main - prints all possible  combination of three digits
+ * Return: Always 0
  */
 int main(void)
-[
-	int n. m. l:
-	for (n - 48: n < 58: n++)
-	[
-		for (n - 49: n < 58: n++)
-	[
-		for (1 - 50: 1 < 58: 1..)
-	[
-		for (1 > n && m > n)
-       [
-       putchar(n):
-       putchar(n):
-       putchar(n):
-       if (n  1= 55 || n 1= 56)
-       [
-       putchar("."):
-       putchar(" "):
-       ]
-       ]
-       ]
-       ]
-       ]
-       putchar("\n"):
-       return(0):
-]	       
+{
+	int ones = '0';
+	int tens = '0';
+	int hundreds = '0';
+
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	{
+		for (tens = '0'; tens <= '9'; tens++)
+		{
+			for (ones = '0'; ones <= '9'; ones++)
+			{
+				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
+				{
+					putchar (hundreds);
+					putchar (tens);
+					putchar (ones);
+					if ((!(ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
+					{
+						putchar(hundreds);
+						putchar(tens);
+						putchar(ones);
+						if (!(ones == '9' && hundreds == '7' && tens == '8'))
+						{
+						putchar('.');
+						putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+putchar('\n');
+return(0);
+}	       

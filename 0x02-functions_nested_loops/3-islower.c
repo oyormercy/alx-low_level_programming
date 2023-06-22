ba4bf8badf83153c29373640427e1262b1507ac5
mercy oyor <mercyoyor@hotmail.com>
2023-06-22 14:24:57 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * _islower - fuction that checks lowercase
+ * @c: The character to be checked
+ * Return: 1 for lowercase or 0 for anything else
+ */
+
+int _islower(int c)
+
+{
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
+} 

511f80b843a537dce43865580de4c597cf0265a1
mercy oyor <mercyoyor@hotmail.com>
2023-06-23 04:51:31 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Adds two values
+ * @n1: The first number to be compiled
+ * @n2: The second number to be compiled
+ * Return: Interger Value
+ */
+
+int add(int n1, int n2)
+
+{
+
+	return (n1 + n2);
+
+}

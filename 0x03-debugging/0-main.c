6d6f6d4253cc56f16eba63104915e5f680d4f883
mercy oyor <mercyoyor@hotmail.com>
2023-06-24 14:05:44 -0700

    1-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main positive_or_negative - Test function for negative or positive
+ * Return: 0 always
+ */
+
+int main positive_or_negative(void)
+
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+return (0);
+}
+	
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN.H
+#define MAIN.H
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int months, int days, int years);
+int convert_days(int month, int days);
+
+#endif
